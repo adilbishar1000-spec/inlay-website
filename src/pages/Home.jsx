dbef17433da6ae7516197746ee9c2475dea4ef54
adilbishar <adilbishar1000@gmail.com>
2026-05-18 03:12:21 +0530

    updated craft page animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import TrendingProducts from '../components/TrendingProducts';
 const Home = () => {
   return (
     <>
+    <div className="space-y-12 md:space-y-20"></div>
       <Hero />
       <CategoriesSlider />
       <TrendingProducts />
